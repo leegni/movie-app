53a994d8c658a4240a0b7b6584faf3a743d429bb
Geunhee Lee <igni950424@gmail.com>
2022-03-07 00:39:42 +0900

    0306 movie-app

---
+++ src/routes/Detail.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
+    Link
+  } from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Detail(){
+    const {id}= useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+
+    const getMovie = async () => {
+        const json = await(await fetch(
+            `https://yts.mx/api/v2/movie_details/json?movie_id=${id}`
+            )).json();         
+           setMovie(json.data.movie);
+           setLoading(false);
+    };
+
+    useEffect(()=>{
+      getMovie();
+    },[]);
+    
+   
+    return (
+        <div>
+        {loading ? (
+          <h1>Loading...</h1>
+          ) : (
+            <div>
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                coverImg={movie.large_cover_image}
+                title={movie.title}
+                summary={movie.description_full}
+                genres={movie.genres}
+              />
+            </div>)}
+      </div>
+    );
+}
+export default Detail;
